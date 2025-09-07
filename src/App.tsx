576898a3b69628fec7c46d7c566afdfa13fc3711
[dyad] <git@dyad.sh>
2025-09-08 01:54:31 +0530

    [dyad] Resolved bundling error by removing an unused dependency - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import { WalletProvider } from "./context/WalletContext";
@@ -9,23 +8,19 @@ import { AnimatedRoutes } from "./components/AnimatedRoutes";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./lib/apollo";
 
-const queryClient = new QueryClient();
-
 const App = () => (
   <ApolloProvider client={client}>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <WalletProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Layout>
-              <AnimatedRoutes />
-            </Layout>
-          </BrowserRouter>
-        </WalletProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <WalletProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Layout>
+            <AnimatedRoutes />
+          </Layout>
+        </BrowserRouter>
+      </WalletProvider>
+    </TooltipProvider>
   </ApolloProvider>
 );
 
