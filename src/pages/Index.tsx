6fc7cdef997fde3b8af510d41840033ac02ca193
[dyad] <git@dyad.sh>
2025-09-08 01:55:22 +0530

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ import { EmptyState } from "@/components/EmptyState";
 import { fetchFromIPFS } from "@/lib/ipfs";
 import { motion } from "framer-motion";
 import { AnimatedCounter } from "@/components/AnimatedCounter";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client/react";
 
 const GET_ELECTIONS = gql`
   query GetElections {
