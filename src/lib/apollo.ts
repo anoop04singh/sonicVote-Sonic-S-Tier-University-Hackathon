d29b4c2b5fc5d4b2d64c784aab836abc4b3584fa
[dyad] <git@dyad.sh>
2025-09-08 13:15:52 +0530

    Updated src/lib/apollo.ts

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,13 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
+const GRAPH_API_KEY = import.meta.env.VITE_GRAPH_API_KEY;
+
+if (!GRAPH_API_KEY) {
+  console.error("VITE_GRAPH_API_KEY is not set. Please add it to your .env.local file.");
+}
+
 const httpLink = new HttpLink({
-  uri: "https://api.studio.thegraph.com/query/120348/sonic-vote/version/latest",
+  uri: `https://gateway.thegraph.com/api/${GRAPH_API_KEY}/subgraphs/id/DhHm4E3T5YNhT6VxubamJCU8bAFNL48SkeRtamLvEt9p`,
 });
 
 export const client = new ApolloClient({
